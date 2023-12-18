7f613fa23984fe7b2a57e1d90e0ba9047a17010c
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2023-12-18 20:30:40 +0530

    org name corrected DEVOPS-21

--- github_rest_api.sh
+++ github_rest_api.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 curl --request GET \
---url "https://api.github.com/devwothkrishna" \
+--url "https://api.github.com/devwithkrishna" \
 --header "Authorization: Bearer $GITHUB_TOKEN" \
 --header "X-GitHub-Api-Version: 2022-11-28"
